d6a75410cd46b67a6e2722e47be831f790017a27
KieArm <87212808+KieArm@users.noreply.github.com>
2022-03-09 15:04:28 +0800

    09/03/2022
    equipment.html now considered completed.
    Added equipment icon assets.
    Setup JavaScript for orders/rewards sections

--- js/index.js
+++ js/index.js
@@ -2,34 +2,55 @@ const containerFrame = document.querySelector('.content-container');
 const navLink = document.querySelectorAll('.nav-link');
 let contentFrame = document.querySelector('.content-frame');
 let menuIcon = document.querySelectorAll('.menu-icon');
+let menuSelect = document.querySelectorAll('.orders-menu');
 
+// First load dom init
 navLink.forEach((link) => {
   link.addEventListener('click', e => {
     let url = e.currentTarget.dataset.url;
     
-    // Fetch page data
+    // Fetch data for new dom content
     fetch(`${url}.html`)
       .then((response) => {
         return response.text();
       })
       .then((html) => {
         containerFrame.innerHTML = html
-        // Reset values & add event listener
-        contentFrame = document.querySelector('.content-frame');
-        menuIcon = document.querySelectorAll('.menu-icon');
-        menuIcon.forEach((icon) => {
-          icon.addEventListener('click', e => {
-            let url = e.currentTarget.dataset.url;
-          
-            fetch(`${url}.html`)
+        // Reset dom values & add event listeners
+        // Select listener type depending on page content
+        if (url === 'orders' || url === 'rewards') {
+          contentFrame = document.querySelector('.content-frame');
+          menuSelect = document.querySelectorAll('.orders-menu');
+          menuSelect.forEach((option) => {
+            option.addEventListener('change', e => {
+            let url = e.target.value;
+  
+              fetch(`${url}.html`)
               .then((response) => {
                 return response.text();
               })
               .then((html) => {
                 contentFrame.innerHTML = html
               })
+            })
           })
-        })
+        } else {
+          contentFrame = document.querySelector('.content-frame');
+          menuIcon = document.querySelectorAll('.menu-icon');
+          menuIcon.forEach((icon) => {
+            icon.addEventListener('click', e => {
+            let url = e.currentTarget.dataset.url;
+
+              fetch(`${url}.html`)
+              .then((response) => {
+                return response.text();
+              })
+              .then((html) => {
+                contentFrame.innerHTML = html
+              })
+            })
+          })
+        }
       })
   })
 });
