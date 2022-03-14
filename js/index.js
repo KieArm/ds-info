fb4e41190b3e31cef73379169d951ce51825f04f
KieArm <87212808+KieArm@users.noreply.github.com>
2022-03-14 12:31:54 +0800

    14/03/2022
    Configured responsive design
    Added index content
    Added links to repo/portfolio
    
    Updated various comments

--- js/index.js
+++ js/index.js
@@ -1,56 +1,56 @@
-const containerFrame = document.querySelector('.content-container');
-const navLink = document.querySelectorAll('.nav-link');
-let contentFrame = document.querySelector('.content-frame');
-let menuIcon = document.querySelectorAll('.menu-icon');
-let menuSelect = document.querySelectorAll('.orders-menu');
+const containerFrame = document.querySelector(".content-container");
+const navLink = document.querySelectorAll(".nav-link");
+let contentFrame = document.querySelector(".content-frame");
+let menuIcon = document.querySelectorAll(".menu-icon");
+let menuSelect = document.querySelectorAll(".orders-menu");
 
-// First load dom init
+// First load dom init for navbar
 navLink.forEach((link) => {
-  link.addEventListener('click', e => {
+  link.addEventListener("click", (e) => {
     let url = e.currentTarget.dataset.url;
-    
-    // Fetch data for new dom content
+
+    // Fetch data for dom content update
     fetch(`/data/${url}.html`)
       .then((response) => {
         return response.text();
       })
       .then((html) => {
-        containerFrame.innerHTML = html
-        // Reset dom values & add event listeners
-        // Select listener type depending on page content
-        if (url === 'orders' || url === 'rewards') {
-          contentFrame = document.querySelector('.content-frame');
-          menuSelect = document.querySelectorAll('.orders-menu');
+        containerFrame.innerHTML = html;
+        // Reset dom values & add listener for dropdown menu changes
+        if (url === "orders" || url === "rewards") {
+          contentFrame = document.querySelector(".content-frame");
+          menuSelect = document.querySelectorAll(".orders-menu");
           menuSelect.forEach((option) => {
-            option.addEventListener('change', e => {
-            let url = e.target.value;
-  
-              fetch(`/data/${url}.html`)
-              .then((response) => {
-                return response.text();
-              })
-              .then((html) => {
-                contentFrame.innerHTML = html
-              })
-            })
-          })
-        } else {
-          contentFrame = document.querySelector('.content-frame');
-          menuIcon = document.querySelectorAll('.menu-icon');
-          menuIcon.forEach((icon) => {
-            icon.addEventListener('click', e => {
-            let url = e.currentTarget.dataset.url;
+            option.addEventListener("change", (e) => {
+              let url = e.target.value;
 
               fetch(`/data/${url}.html`)
-              .then((response) => {
-                return response.text();
-              })
-              .then((html) => {
-                contentFrame.innerHTML = html
-              })
-            })
-          })
+                .then((response) => {
+                  return response.text();
+                })
+                .then((html) => {
+                  contentFrame.innerHTML = html;
+                });
+            });
+          });
+          // Reset dom values & add listener for clickable icons
+        } else {
+          contentFrame = document.querySelector(".content-frame");
+          menuIcon = document.querySelectorAll(".menu-icon");
+          menuIcon.forEach((icon) => {
+            icon.addEventListener("click", (e) => {
+              let url = e.currentTarget.dataset.url;
+
+              fetch(`/data/${url}.html`)
+                .then((response) => {
+                  return response.text();
+                })
+                .then((html) => {
+                  contentFrame.innerHTML = html;
+                });
+            });
+          });
         }
-      })
-  })
+      });
+  });
 });
